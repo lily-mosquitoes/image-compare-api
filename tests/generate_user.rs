060f6e67d4e2ffb9869034ee885456b8c2cc5b8b
Lílian Ferreira de Freitas <lily.mosquitoes@gmail.com>
2024-05-19 15:49:01 +0300

    test: move generate user tests to own file

--- tests/generate_user.rs
+++ tests/generate_user.rs
@@ -26,30 +26,27 @@ struct User {
     average_lambda: f64,
 }
 
-#[sqlx::test(fixtures(path = "./../fixtures", scripts("users")))]
-async fn get_user_with_correct_id_returns_200_ok(
+#[sqlx::test]
+async fn generate_user_returns_201_created(
     _: SqlitePoolOptions,
     db_options: SqliteConnectOptions,
 ) {
     let client = get_asynchronous_api_client(STATIC_DIR, db_options).await;
 
-    let response = client
-        .get(uri!("/api/user/3fa85f64-5717-4562-b3fc-2c963f66afa6"))
-        .dispatch()
-        .await;
+    let response = client.post(uri!("/api/user")).dispatch().await;
 
-    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.status(), Status::Created);
 }
 
-#[sqlx::test(fixtures(path = "./../fixtures", scripts("users")))]
-async fn get_user_with_correct_id_is_json_ok_response(
+#[sqlx::test]
+async fn generate_user_is_json_ok_response(
     _: SqlitePoolOptions,
     db_options: SqliteConnectOptions,
 ) {
     let client = get_asynchronous_api_client(STATIC_DIR, db_options).await;
 
     let body = client
-        .get(uri!("/api/user/3fa85f64-5717-4562-b3fc-2c963f66afa6"))
+        .post(uri!("/api/user"))
         .dispatch()
         .await
         .into_json::<OkResponse<User>>()
@@ -58,15 +55,15 @@ async fn get_user_with_correct_id_is_json_ok_response(
     assert!(body.is_some());
 }
 
-#[sqlx::test(fixtures(path = "./../fixtures", scripts("users")))]
-async fn get_user_with_correct_id_returns_expected_user(
+#[sqlx::test]
+async fn generate_user_returns_new_user(
     _: SqlitePoolOptions,
     db_options: SqliteConnectOptions,
 ) {
     let client = get_asynchronous_api_client(STATIC_DIR, db_options).await;
 
     let body = client
-        .get(uri!("/api/user/3fa85f64-5717-4562-b3fc-2c963f66afa6"))
+        .post(uri!("/api/user"))
         .dispatch()
         .await
         .into_json::<OkResponse<User>>()
@@ -74,10 +71,36 @@ async fn get_user_with_correct_id_returns_expected_user(
         .expect("body to be present");
 
     let expected_user = User {
-        id: Uuid::parse_str("3fa85f64-5717-4562-b3fc-2c963f66afa6").unwrap(),
-        comparisons: 1,
-        average_lambda: 0.1234,
+        id: body.data.id,
+        comparisons: 0,
+        average_lambda: 0.0,
     };
 
     assert_eq!(body.data, expected_user);
+}
+
+#[sqlx::test]
+async fn generate_user_is_not_idempotent(
+    _: SqlitePoolOptions,
+    db_options: SqliteConnectOptions,
+) {
+    let client = get_asynchronous_api_client(STATIC_DIR, db_options).await;
+
+    let body_a = client
+        .post(uri!("/api/user"))
+        .dispatch()
+        .await
+        .into_json::<OkResponse<User>>()
+        .await
+        .expect("body to be present");
+
+    let body_b = client
+        .post(uri!("/api/user"))
+        .dispatch()
+        .await
+        .into_json::<OkResponse<User>>()
+        .await
+        .expect("body to be present");
+
+    assert_ne!(body_a.data.id, body_b.data.id);
 }
